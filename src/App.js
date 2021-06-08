d20d0bbb7a853cb64b7bef79cfa7c1262daf8279
Joel Connell <joel.connell@yahoo.com>
2021-06-08 11:44:45 -0700

    removed header temporarially.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React, { Component } from 'react';
 import Main from './Main.js';
-import Header from './Header.js';
+//import Header from './Header.js';
 class App extends React.Component{
 render(){
   
@@ -10,7 +10,8 @@ render(){
   
   return(
   <>
-<Header></Header>
+{//<Header></Header>
+}
 <Main></Main>
 <footer><h3>Author: Joel Connell</h3></footer>
 
