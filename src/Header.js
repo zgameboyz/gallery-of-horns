c6e93b1c8b394e1744830adfc5e98b3b0911eb52
Joel Connell <joel.connell@yahoo.com>
2021-06-09 22:01:45 -0700

    failed at making the modal work

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,16 @@
 import React from 'react';
+import HornedBeast from './HornedBeast';
+import data from './data.json';
+import { Card } from 'react-bootstrap';
+import CardColumns from 'react-bootstrap/CardColumns'
 
-
-
+class Header extends React.Component{
+render(){
+  return(
+    <>
 <h1>This is the Lab 1 Gallery of Horns</h1>
-
+  
+</>
+  )}
+}
+export default Header;
